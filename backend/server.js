fb5f200e0ec41601e929ec60c42a54097a8702eb
unknown <cherguidouaameriem@gmail.com>
2026-05-12 19:58:20 +0200

    change cors prblm

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(
     credentials: true,
   })
 );
-  app.options("*", cors());
+  app.options(/.*/, cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // 🔥 ROUTES
